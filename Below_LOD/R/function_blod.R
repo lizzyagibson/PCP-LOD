6c12a4f1ab56ad429920a6d86e0ab2fa07372661
lizzyagibson <eag2186@cumc.columbia.edu>
2019-07-19 14:49:35 -0400

    Add pcp_load function

--- Below_LOD/R/function_blod.R
+++ Below_LOD/R/function_blod.R
@@ -15,8 +15,8 @@ library(tidyverse)
 # if Y < c, push to zero
 prox_l1 <- function(Y, c) {
   
-    myzero <- matrix(data = 0, ncol = ncol(v), nrow = nrow(v))
-    X <- sign(v) * pmax(abs(v) - lambda, myzero)
+    myzero <- matrix(data = 0, ncol = ncol(Y), nrow = nrow(Y))
+    X <- sign(Y) * pmax(abs(Y) - c, myzero)
     X
     } 
 
@@ -36,7 +36,7 @@ prox_nuclear <- function(Y,c) {
   S_new <- sign(S) * pmax(abs(S) - c, myzero)
   # Threshold the singular values, if SV < c, push it to zero
   
-  X <- U %*% S_new %*% t(V)
+  X <- U %*% diag(S_new) %*% t(V)
   
   nuclearX  <- sum(abs(S_new))
   # This is the L1 of the truncated singular values
@@ -84,6 +84,98 @@ loss_lod <- function(X, D, Delta) {
 
 ############################################################
 ############################################################
+
+# % If the LOD threshold Delta = 0, solve the following ADMM splitting problem:
+#   % min_{L1,L2,L3,S1,S2}
+# %      ||L1||_* + lambda * ||S1||_1 + mu/2 * ||L2+S2-D||_F^2 + I_{L3>=0}
+# % s.t. L1 = L2
+# %      L1 = L3
+# %      S1 = S2.
+# %
+# % If Delta is not 0, replace ||L2+S2-D||_F^2 with LOD penalty.
+# %
+# % Below-LOD data input in D should be denoted as negative values, e.g. -1.
+
+pcp_lod <- function(D, lambda, mu, Delta) {
+  
+  m <- nrow(D)
+  n <- ncol(D)
+  rho <- 1 # Augmented Lagrangian coefficient (rate)
+  
+  L1 <- matrix(0, m, n)
+  L2 <- matrix(0, m, n)
+  L3 <- matrix(0, m, n)
+  
+  S1 <- matrix(0, m, n)
+  S2 <- matrix(0, m, n)
+  
+  Z1 <- matrix(0, m, n)
+  Z2 <- matrix(0, m, n)
+  Z3 <- matrix(0, m, n)
+  
+  MAX_ITER <- 100
+  LOSS_THRESH <- 1e-5
+  SAME_THRESH <- 1e-4
+  
+  loss <- vector("numeric", MAX_ITER)
+  
+  for (i in 1:MAX_ITER) {
+    nuc <- prox_nuclear( (L2 + L3 -(Z1 + Z2) / rho) / 2, 1/2/rho)
+    L1 <- nuc[[1]]
+    nuclearL1 <- nuc[[2]]
+    S1 <- prox_l1( S2 - Z3 / rho, lambda / rho)
+    
+    L2_opt1 <- ((m*rho*D)     + (mu + rho)*Z1 - (mu*Z3) + (mu + rho)*rho*L1 - (mu*rho*S1)) / ((2*mu*rho) + rho^2)
+    L2_opt2 <- L1 + Z1/rho
+    L2_opt3 <- ((m*rho*Delta) + (mu + rho)*Z1 - (mu*Z3) + (mu + rho)*rho*L1 - (mu*rho*S1)) / ((2*mu*rho) + rho^2)
+    L2_opt4 <- (                (mu + rho)*Z1 - (mu*Z3) + (mu + rho)*rho*L1 - (mu*rho*S1)) / ((2*mu*rho) + rho^2)
+    
+    L2_new <- L2_opt1 * (D >= 0) +
+              L2_opt2 * (D < 0 & (L2 + S2) >= 0 & (L2 + S2) <= Delta) +
+              L2_opt3 * (D < 0 & (L2 + S2) > Delta) +
+              L2_opt4 * (D < 0 & (L2 + S2) < 0)
+    
+    S2_opt1 <- ((mu*rho*D)     + (mu + rho)*Z3 - (mu*Z1) + (mu + rho)*rho*S1 - (mu*rho*L1)) / ((2*mu*rho) + rho^2)
+    S2_opt2 <- S1 + Z3/rho
+    S2_opt3 <- ((mu*rho*Delta) + (mu + rho)*Z3 - (mu*Z1) + (mu + rho)*rho*S1 - (mu*rho*L1)) / ((2*mu*rho) + rho^2)
+    S2_opt4 <- (                 (mu + rho)*Z3 - (mu*Z1) + (mu + rho)*rho*S1 - (mu*rho*L1)) / ((2*mu*rho) + rho^2)
+    
+    S2 <- S2_opt1 * (D >= 0) +
+          S2_opt2 * (D < 0 & (L2 + S2) >= 0 & (L2 + S2) <= Delta) +
+          S2_opt3 * (D < 0 & (L2 + S2) > Delta) +
+          S2_opt4 * (D < 0 & (L2 + S2) < 0)
+  
+    L2 <- L2_new
+    
+    L3 <- max(L1 + L2/rho, 0)
+    
+    Z1 <- Z1 + rho*(L1 - L2)
+    Z2 <- Z2 + rho*(L1 - L3)
+    Z3 <- Z3 + rho*(S1 - S2)
+    
+    loss[i] <- nuclearL1 + lambda*sum(abs(S1)) +
+      mu*loss_lod((L2 + S2), D, Delta) +
+      sum(Z1*(L1 - L2)) +
+      sum(Z2*(L1 - L3)) +
+      sum(Z3*(S1-S2)) +
+      ((rho/2) * (sum((L1-L2)^2))) +
+      sum((L1 - L3)^2) +
+      sum((S1 - S2)^2)
+    
+    if (i != 1 && 
+        abs(loss[i-1] - loss[i]) < LOSS_THRESH && 
+        is_same(SAME_THRESH, L1, L2, L3) &&
+        is_same(SAME_THRESH, S1, S2)) {
+      break}
+  }
+  
+  L <- (L1 + L2 + L3) / 3
+  S <- (S1 + S2) / 2
+  list(L = L, S = S, loss = loss)
+}
+
+fake <- matrix(rnorm(100), 10)
+
 
 
 
