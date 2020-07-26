b70c30b72cd9bb098e32e7a2a3ca83cefcc194b6
lizzyagibson <eag2186@cumc.columbia.edu>
2020-07-25 20:24:01 -0400

    Make comparison figures.

---
+++ jsm_figures.R
@@ -0,0 +1,57 @@
+n = 1000
+r = 3
+p = 20
+
+U = matrix(runif(n*r), nrow = n, ncol = r)
+V = matrix(runif(r*p), nrow = r, ncol = p)
+
+L = U %*% V
+apply(L, 2, sd)
+sd(L)
+var(as.vector(L))
+
+as_tibble(L) %>% 
+  mutate(id = 1:nrow(L)) %>% 
+  pivot_longer(V1:V20) %>% 
+  #filter(name == "V1") %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram() +
+  facet_wrap(~name)
+
+as_tibble(L) %>% 
+  mutate(id = 1:nrow(L)) %>% 
+  pivot_longer(V1:V20) %>% 
+  #filter(name == "V1") %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram(bins = 50, fill = "#1976D2") + # HTML color code to match latex
+  theme_minimal() +
+  labs(x = "Simulated Variable", y = "Count")
+
+Z = matrix(rnorm(n*p, mean = 0, sd = .5*sd(L)), nrow = n, ncol = p)
+X = L + Z
+D = pmax(X, 0)
+apply(D, 2, sd)
+apply(D, 2, max)
+sd(D)
+
+sim_hist <- as_tibble(L) %>% 
+  mutate(id = 1:nrow(L)) %>% 
+  pivot_longer(V1:V20) %>% 
+  #filter(name == "V1") %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram(bins = 50, fill = "#1976D2") + # HTML color code to match latex
+  theme_minimal() +
+  labs(x = "Simulated Low Rank Matrix", y = "Count")
+
+#library(GGally)
+sim_corr <- ggcorr(as_tibble(D), limits = FALSE,
+       hjust = 0.85, size = 3, layout.exp = 1) +
+  labs(x = "Simulated Correlation Matrix")
+
+pdf("/Users/lizzy/Summer 2020/JSM PCP-LOD/figures/jsm_hist.pdf")
+sim_hist
+dev.off()
+
+pdf("/Users/lizzy/Summer 2020/JSM PCP-LOD/figures/jsm_corr.pdf")
+sim_corr
+dev.off()
---
+++ plot_M_relerror.R
@@ -0,0 +1,80 @@
+# Plot PCP-LOD, LOD/sqrt(2), PCA resulting error
+# Across rank, sigma, mu, and LOD
+library(tidyverse)
+library(R.matlab)
+
+rank = c(1, 2, 3, 4, 5)
+sigma = c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, "1.0")
+delta = c(0.05,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7)
+
+# r = 1
+# d = 0.3
+# s = 0.1
+
+pcp_out <- tibble()
+
+for (d in delta) {
+for (s in sigma) {
+#for (r in rank) {
+  # Mu without sigma
+  
+  read <- readMat(here::here(paste0("HPC_PCP/data_lod_", d, "_rank_", r, 
+                                    "_sigma_", s, ".mat")))
+  
+  relerror <- read[[1]] %>% 
+    as_tibble() %>% rename("relerror_PCA" =1, "relerror_PCP_sqrt2" =2, "relerror_PCP_LOD" =3)
+  
+  # score_error <- read[[3]] %>% 
+  #   as_tibble() %>% rename("score_relerror_PCA" =1, "score_relerror_PCP_sqrt2" =2, 
+  #                          "score_relerror_PCP_LOD" =3) 
+  # 
+  # pattern_error <- read[[4]] %>% 
+  #   as_tibble() %>% rename("pattern_relerror_PCA" =1, "pattern_relerror_PCP_sqrt2" =2, 
+  #                          "pattern_relerror_PCP_LOD" =3) 
+  out_mu <- read[[2]] %>% 
+    as_tibble() %>% rename("belowlod_relerror_PCA" =1, "belowlod_relerror_PCP_sqrt2" =2, 
+                           "belowlod_relerror_PCP_LOD" =3) %>% 
+    mutate(rank = r,
+           sigma = s,
+           delta = d,
+           iter = 1:100) %>% 
+    cbind(., relerror) %>% as_tibble()
+  
+  
+  pcp_out <- rbind(pcp_out, out_mu) %>% drop_na(.)
+  }
+}
+#}
+
+pcp_plot <- pcp_out %>% 
+  pivot_longer(grep("relerror", colnames(.)),
+               values_to = "rel_error") %>% 
+  mutate(type = ifelse(grepl("belowlod", name), "belowlod", 
+                       ifelse(grepl("score", name), "score",
+                       ifelse(grepl("pattern", name), "pattern", "overall"))),
+         model = ifelse(grepl("PCA", name), "PCA",
+                        ifelse(grepl("sqrt2", name), "PCP_sqrt2", "PCP_LOD")),
+         rank = as.factor(rank),
+         sigma = as.factor(sigma),
+         delta = as.factor(delta)) %>% 
+  select(-name)
+
+names(pcp_plot)
+
+# rank increase -- PCP_LOD does better
+pcp_plot %>% 
+  filter(model != "PCA") %>% 
+  #filter(type != "overall") %>% 
+  #filter(type != "belowlod") %>% 
+  filter(sigma == "1.0") %>% 
+  ggplot(aes(x = delta, y = rel_error, color = model, linetype = sigma)) +
+  geom_boxplot(notch = FALSE, outlier.shape = NA) +
+  facet_grid(~type) + #lims(y = c(0, 3)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "bottom")
+
+
+
+
+
