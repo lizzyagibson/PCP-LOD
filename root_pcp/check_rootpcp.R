102878ddc33dc6c6a9a2d36141690c4c420cb6cc
lizzyagibson <eag2186@cumc.columbia.edu>
2020-10-18 21:20:15 -0400

    Check pcpr function update.

--- root_pcp/check_rootpcp.R
+++ root_pcp/check_rootpcp.R
@@ -1,7 +1,8 @@
 library(R.matlab)
 library(tidyverse)
-
-mixture <- readMat("/Users/lizzy/Principle.Component.Pursuit/Data/mixtures_data.mat")
+library(pcpr)
+library(tictoc)
+mixture <- readMat("/Users/lizzy/Principal.Component.Pursuit/Data/mixtures_data.mat")
 
 mixture_data <- as.data.frame(mixture) %>% 
   as_tibble() %>% dplyr::select(Al, As, Ba, bc, Br, Ca, Cl,
@@ -9,12 +10,25 @@ mixture_data <- as.data.frame(mixture) %>%
                          Ti,  V, Zn) %>% as.matrix()
 
 X <- mixture_data[complete.cases(mixture_data),]
-X[1:5, 1:5]
+# X[1:5, 1:5]
 
 m <- nrow(X)
 n <- ncol(X)
 
-root_out = root_pcp_nonnegL(X, 1/sqrt(m), 10)
+tic()
+root_out = pcp_lod(X, 1/sqrt(m), 10, 0)
+toc()
+
+tic()
+root_out = root_pcp(X, 1/sqrt(m), 10)
+toc()
+
+tic()
+root_nn = root_pcp_nonnegL(X, 1/sqrt(m), 10)
+toc()
 
 norm(root_out[[1]], "F")
 norm(root_out[[2]], "F")
+
+norm(root_nn[[1]], "F")
+norm(root_nn[[2]], "F")
